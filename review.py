b091aec9db53d0da8b1d7dfd3112c12ebdcdd1c0
Shan Yu <dereklutw@gmail.com>
2018-07-25 15:27:21 +0800

    add word_count option

--- review.py
+++ review.py
@@ -8,14 +8,54 @@ with open('reviews.txt', 'r') as f:
 			print(len(data))
 print('檔案讀取完了, 總共有', len(data), '筆資料')
 
-sum_len = 0
-for d in data:
-	sum_len += len(d)     #sum_len = sum_len + len(d)  
+print(data[0])
 
-print('留言平均長度是:', sum_len/len(data))	
-
-new = []	#做對留言的篩選
+wc = {}	# word_count
 for d in data:
-	if len(d) < 100:
-		new.append(d)
-print('一共有', len(new), '筆留言長度小於100')		
+	words = d.split()
+	for word in words:
+		if word in wc:
+			wc[word] += 1
+
+		else:	
+			wc[word] = 1	#新增新的key進wc字典
+
+for word in wc:
+	if wc[word] > 1000000:
+		print(word, wc[word])		
+
+print(len(wc))		
+
+while True:
+	word = input('請問你想查甚麼字: ')
+	if word == 'q':
+		break
+	if word in wc:	
+		print(word, '出現過的次數為: ', wc[word])
+	else:
+		print('這個字沒有出現過喔')
+
+print('感謝使用查詢功能')	
+ 	
+	
+
+#	sum_len = 0
+#	for d in data:
+#		sum_len += len(d)     #sum_len = sum_len + len(d)  
+
+#	print('留言平均長度是:', sum_len/len(data))	
+
+#	for d in data:
+#		if len(d) < 100:
+#			new.append(d)
+#	print('一共有', len(new), '筆留言長度小於100')		
+
+
+#	good = []				#清單速寫法 good = [d for d in data if 'good' in d]
+#	for d in data:
+#		if 'good' in d:
+#			good.append(d)
+#	print('一共有', len(good), '筆留言提到good')
+
+
+
